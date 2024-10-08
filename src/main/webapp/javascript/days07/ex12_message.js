0c8a235edd1bd739a63119577cb71ba579de3fc0
woals5636 <woals5636@naver.com>
2024-10-08 17:34:22 +0900

    1008javascript

---
+++ src/main/webapp/javascript/days07/ex12_message.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * 함수를 만들어서 내보내기
+ */
+
+const message = () => {
+    const name = "홍길동";
+    const age = 20;
+    return `${name} / ${age}`;
+};
+
+export default message;
---
+++ src/main/webapp/javascript/days07/ex12_person.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 변수 내보내기
+ * 
+ */
+/*
+export const name = "admin";
+export const age = 20;
+*/
+
+const name = "admin";
+const age = 20;
+
+export{ name, age };
